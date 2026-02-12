ee72daabe05e2d7287560ec236cdb077b0604bff
BoaznCestroyer <151847751+BoazChdestroyer@users.noreply.github.com>
2026-02-12 13:50:36 +0000

    frewf

--- 1v1-airhockey.py
+++ 1v1-airhockey.py
@@ -2,22 +2,15 @@ import pygame
 from pygame.locals import *
 pygame.init()
 
-
 pygame.display.set_caption("1v1 Air Hockey")
 clock = pygame.time.Clock()
-HEIGHT, WIDTH = 800, 600
+WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 ICE = (180, 220, 255)
 WHITE = (255, 255, 255)
 RED = (220, 50, 50)
 BLUE = (50, 100, 220)
-
-screen.fill(ICE)
-pygame.draw.circle(screen, WHITE, [HEIGHT // 2, WIDTH // 2], 10, 0)
-pygame.draw.circle(screen, WHITE, [HEIGHT // 2, WIDTH // 2], 50, 2)
-pygame.draw.line(screen, WHITE, [WIDTH // 2, 0], [WIDTH // 2, HEIGHT], 2)
-pygame.draw.rect(screen, WHITE, [0, 0, WIDTH, HEIGHT], 2)
 
 pygame.display.update()
 
@@ -27,7 +20,12 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
-    screen.fill((0, 0, 0))
+    screen.fill(ICE)
+    pygame.draw.circle(screen, WHITE, [HEIGHT // 2, WIDTH // 2], 10, 0)
+    pygame.draw.circle(screen, WHITE, [HEIGHT // 2, WIDTH // 2], 50, 2)
+    pygame.draw.line(screen, WHITE, [WIDTH // 2, 0], [WIDTH // 2, HEIGHT], 2)
+    pygame.draw.rect(screen, WHITE, [0, 0, WIDTH, HEIGHT], 2)
+    
     pygame.display.flip()
     clock.tick(60)
 
