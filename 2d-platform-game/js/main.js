a27c479b01fb99ad306d474e52412d4b82c9d3b1
BinckT <162120198+BinckT@users.noreply.github.com>
2026-03-08 21:46:02 +0000

    onnodige bestanden verwijderd

--- 2d-platform-game/js/main.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', () => {
-    const canvas = document.getElementById('gameCanvas');
-    canvas.width = CANVAS_W;
-    canvas.height = CANVAS_H;
-
-    const game = new Game(canvas);
-    game.start();
-});
